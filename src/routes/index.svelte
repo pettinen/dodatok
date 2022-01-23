f8d743132442439ee1369ec2cf36f8868e701b85
Jeremias Pettinen <hi@jeremi.as>
2022-01-23 06:28:33 +0200

    Update tsconfig and eslintrc

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-  const a = function(): string {
-    return "die";
-  };
+  const welcome = "die";
 </script>
 
-<h1>Welcome to {a()}</h1>
+<h1>Welcome to {welcome}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> 
